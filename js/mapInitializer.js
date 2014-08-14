5fa65a10bd169e16090808d4a1f9090fffd5ea68
Adam Estela <adamestela@gmail.com>
2014-08-14 03:03:59 -0700

    completed prototype frontend

--- js/mapInitializer.js
+++ js/mapInitializer.js
@@ -1,11 +1,11 @@
 // Initializer is called immediately and all variables are contained. 
-(function MapInitializer(){
+function MapInitializer($scope){
 	var self = this;
 
 	// Store earthquake location data
-	var data = [
-		{lat:-40.397, lng:190.644}, 
-		{lat:-30.397, lng:160.644}
+	$scope.data = [
+		{name:"thing1", lat:-40.397, lng:190.644, size:10}, 
+		{name:"thing2", lat:-30.397, lng:160.644, size:5}
 	];
 
 	// Create and store markers for earthquake
@@ -15,9 +15,9 @@
 	self.map = null;
 
 	self.setPositions = function(){
-		for(var i = 0; i < data.length; ++i){
+		for(var i = 0; i < $scope.data.length; ++i){
 			var marker = new google.maps.Marker({ 
-				position: new google.maps.LatLng(data[i].lat, data[i].lng), 
+				position: new google.maps.LatLng($scope.data[i].lat, $scope.data[i].lng), 
 				map: self.map 
 			});
 
@@ -38,10 +38,10 @@
 		self.setPositions();
 	};
 
-	self.moveToMarker = function(index){
-		self.map.panTo(markers[index].getPosition());
+	$scope.moveToMarker = function($index){
+		self.map.panTo(markers[$index].getPosition());
 	};
 
 	// Add listener to wait for page load.
 	google.maps.event.addDomListener(window, 'load', self.initialize);
-}())
+}
